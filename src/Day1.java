c711c60501ebf8dcb06fa1b2529b6be2cb57f56d
Khowataooi <hadesnd1505@gmail.com>
2023-09-08 16:18:20 +0700

    first commit

---
+++ src/Day1.java
@@ -0,0 +1,201 @@
+public class Day1 {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello world");
+//        System.out.println("Hi ae");
+
+//        System.out.println("Nhập tên:");
+//        Scanner input = new Scanner(System.in);
+//        String name = input.nextLine();
+//        System.out.println("Hello C07");
+
+//        int a = 3;
+//        System.out.println(a);
+
+//        Scanner inputNum = new Scanner(System.in);
+//        int a = inputNum.nextInt();
+//        System.out.println(a);
+
+//        Scanner inputNum = new Scanner(System.in);
+//        System.out.println("Nhập a");
+//        int a = inputNum.nextInt();
+//        System.out.println("Nhập b");
+//        int b = inputNum.nextInt();
+//        System.out.println("Tổng: " + (a + b));
+//        System.out.println("Hiệu: " + (a - b));
+//        System.out.println("Tích: " + (a * b));
+//        System.out.println("Thương: " + (a / b));
+//        System.out.println("Dư: " + (a % b));
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Nhập tên");
+//        String name = input.nextLine();
+//        System.out.println("Nhập tuổi");
+//        int age = input.nextInt();
+//        System.out.println("Xin chào " + name + ", tôi năm nay " + age + " tuổi");
+
+//        int a, b;
+//        a = 6;
+//        b = 2;
+//        a += b;
+//        a -= b;
+//        a *= b;
+//        a /= b;
+//        a++;
+//        --b;
+//        if (a > b) {
+//            System.out.println(a + " lớn hơn " + b);
+//        } else if (a < b) {
+//            System.out.println(a + " nhỏ hơn " + b);
+//        }
+
+        // Bài 1:
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Nhập a");
+//        int a = input.nextInt();
+//        System.out.println("Nhập b");
+//        int b = input.nextInt();
+//        if (a % b == 0) {
+//            System.out.println("a chia hết cho b");
+//        } else {
+//            System.out.println("a không chia hết cho b");
+//        }
+
+        // Bài 2:
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Nhập tuổi");
+//        int age = input.nextInt();
+//        if (age < 15) {
+//            System.out.println("Không đủ tuổi học lớp 10");
+//        } else {
+//            System.out.println("Đủ tuổi học lớp 10");
+//        }
+
+        // Bài 3:
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Nhập a");
+//        int a = input.nextInt();
+//        if (a > 0) {
+//            System.out.println("a lớn hơn 0");
+//        } else if (a == 0) {
+//            System.out.println("a bằng 0");
+//        } else {
+//            System.out.println("a nhỏ hơn 0");
+//        }
+
+        // Bài 4:
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Nhập a");
+//        int a = input.nextInt();
+//        System.out.println("Nhập b");
+//        int b = input.nextInt();
+//        System.out.println("Nhập c");
+//        int c = input.nextInt();
+//        int[] numbers = {a, b, c};
+//        int max = numbers[0];
+//        for (int number : numbers) if (number > max) max = number;
+//        System.out.println(max);
+
+        // Bài 5:
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Điểm kiểm tra:");
+//        double a = input.nextDouble();
+//        System.out.println("Điểm giữa kỳ:");
+//        double b = input.nextDouble();
+//        System.out.println("Điểm cuối kỳ:");
+//        double c = input.nextDouble();
+//        double avg = (a * 0.1) + (b * 0.2) + (c * 0.7);
+//        if(avg >= 8) System.out.println("Học lực giỏi");
+//        else if (avg >= 6.5) System.out.println("Học lực khá");
+//        else if (avg >= 5) System.out.println("Học lực trung bình");
+//        else System.out.println("Học lực yếu");
+
+        // Bài 6:
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Nhập doanh số:");
+//        double sales = input.nextDouble();
+//        double commission = sales * 0.1;
+//        System.out.println(commission);
+
+        // Bài 1:
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Nhập độ C");
+//        double celcius = input.nextDouble();
+//        double fahrenheit =  (double) 9 / 5 * celcius + 32;
+//        System.out.println(fahrenheit);
+
+        // Bài 2:
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Nhập mét");
+//        double meter = input.nextDouble();
+//        double feet = meter * 3.2808;
+//        System.out.println(feet);
+
+        // Bài 3:
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Nhập cạnh");
+//        double edge = input.nextDouble();
+//        double area = edge * edge;
+//        System.out.println(area);
+
+        // Bài 4:
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Nhập cạnh a");
+//        double a = input.nextDouble();
+//        System.out.println("Nhập cạnh b");
+//        double b = input.nextDouble();
+//        double area = a * b;
+//        System.out.println(area);
+
+        // Bài 5:
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Nhập cạnh a");
+//        double a = input.nextDouble();
+//        System.out.println("Nhập cạnh b");
+//        double b = input.nextDouble();
+//        double area = a * b / 2;
+//        System.out.println(area);
+
+        // Bài 6:
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Nhập a");
+//        double a = input.nextDouble();
+//        System.out.println("Nhập b");
+//        double b = input.nextDouble();
+//        double x = -b / a;
+//        System.out.println(x);
+
+        // Bài 7:
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Nhập a");
+//        double a = input.nextDouble();
+//        System.out.println("Nhập b");
+//        double b = input.nextDouble();
+//        System.out.println("Nhập c");
+//        double c = input.nextDouble();
+//        double delta = b * b - 4 * a * c;
+//        if (delta < 0) System.out.println("Phương trình vô nghiệm");
+//        if (delta == 0) System.out.println("Phương trình có hai nghiệm kép x1 = x2 = " + -b / 2 * a);
+//        if (delta > 0)
+//            System.out.println("Phương trình có hai nghiệm x1 = " + (-b - Math.sqrt(delta)) / 2 * a + ", x2 = " + (-b + Math.sqrt(delta)) / 2 * a);
+
+        // Bài 8:
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Nhập tuổi");
+//        int age = input.nextInt();
+//        if (age > 0 && age < 120) System.out.println(age + " tuổi");
+//        else System.out.println("Không phải tuổi người");
+
+        // Bài 9:
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Nhập a");
+//        double a = input.nextDouble();
+//        System.out.println("Nhập b");
+//        double b = input.nextDouble();
+//        System.out.println("Nhập c");
+//        double c = input.nextDouble();
+//        if (a == 0 || b == 0 || c == 0) System.out.println("a, b, c không phải cạnh của một tam giác");
+//        else if (a + b > c && b + c > a && c + a > b) System.out.println("a,b,c là cạnh của một tam giác");
+//        else System.out.println("a, b, c không phải cạnh của một tam giác");
+    }
+}
